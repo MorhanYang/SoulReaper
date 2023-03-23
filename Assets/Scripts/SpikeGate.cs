5f67678206018a13d619a1c023a623e50a68ddbe
PPhive <beijingleowang@gmail.com>
2023-03-23 02:32:15 -0700

    Added Spikes

---
+++ Assets/Scripts/SpikeGate.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeGate : MonoBehaviour
+{
+    public bool open;
+    [SerializeField]
+    private float gap_between_spikes;
+    [SerializeField]
+    private GameObject SpikePrefab;
+
+    [SerializeField]
+    private Sprite SpriteOpen;
+
+    [SerializeField]
+    private Sprite SpriteClosed;
+
+    [SerializeField]
+    private List<GameObject> Spikes;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O)) 
+        {
+            openDoor();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            closeDoor();
+        }
+    }
+
+    public void closeDoor() 
+    {
+        if (open) 
+        {
+            for (int i = 0; i < Spikes.Count; i++) 
+            {
+                SpriteRenderer myRenderer = Spikes[i].GetComponent<SpriteRenderer>();
+                myRenderer.sprite = SpriteOpen;
+            }
+        }
+        open = false;
+    }
+
+    public void openDoor() 
+    {
+        if (!open) 
+        {
+            for (int i = 0; i < Spikes.Count; i++)
+            {
+                SpriteRenderer myRenderer = Spikes[i].GetComponent<SpriteRenderer>();
+                myRenderer.sprite = SpriteClosed;
+            }
+        }
+        open = true;
+    }
+
+
+
+}
