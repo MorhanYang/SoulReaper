65e6c8d5538130a03f3f2155685cd1ab0ee75bbe
MorhanYang <yangfengren07@gmail.com>
2023-03-23 04:07:57 -0400

    Revert "Revert "Add sound founction""
    This reverts commit 65774a75067c42d1a38ce765bca4c8d769a119fc.

--- Assets/Scripts/Soul/RecallingMinion.cs
+++ Assets/Scripts/Soul/RecallingMinion.cs
@@ -18,6 +18,11 @@ public class RecallingMinion : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
             yield return new WaitForFixedUpdate();
         }
+        // play sound
+        SoundManager mySoundManager = SoundManager.Instance;
+        mySoundManager.PlaySoundAt(mySoundManager.transform.position, "Heal", false, false, 1, 1, 100, 100);
+
+
         Destroy(gameObject);
     }
 
