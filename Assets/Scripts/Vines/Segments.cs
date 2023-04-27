d23833f02bd37dfcac9a8df2fd3837ecf942bbd6
PPhive <beijingleowang@gmail.com>
2023-04-27 03:17:50 -0400

    Added VinesPrefab to LeoPuzzle

---
+++ Assets/Scripts/Vines/Segments.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segments : MonoBehaviour
+{
+    public Color MyColor;
+    [SerializeField]
+    GameObject MySpriteObject;
+    [SerializeField]
+    GameObject MyLeaf;
+    SpriteRenderer StemRenderer;
+    SpriteRenderer LeafRenderer;
+    float size;
+
+    bool Dying = false;
+    void Start()
+    {
+        StemRenderer = MySpriteObject.GetComponent<SpriteRenderer>();
+        LeafRenderer = MySpriteObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        MyLeaf.SetActive(false);
+        if (Random.Range(0, 2f) > 1)
+        {
+            MyLeaf.transform.localEulerAngles = new Vector3(0, 0, Random.Range(0, -90f));
+        }
+        else
+        {
+            MyLeaf.transform.localEulerAngles = new Vector3(180, 0, Random.Range(0, -90f));
+        }
+        StartCoroutine(Grow());
+    }
+
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            StartDie();
+        }
+        */
+    }
+
+    IEnumerator Grow()
+    {
+        MyColor = MySpriteObject.GetComponent<SpriteRenderer>().color;
+        if (Random.Range(0, 5f) < 1f)
+        {
+            MyLeaf.SetActive(true);
+            //If succeeds, spawn with leaf
+        }
+        else
+        {
+            MyLeaf.SetActive(false);
+        }
+        size = MySpriteObject.transform.localScale.x;
+        while (MySpriteObject.transform.localScale.x < 1)
+        {
+            if (Dying) 
+            {
+                break;
+            }
+            size += Time.deltaTime * 2;
+            MySpriteObject.transform.localScale = new Vector3(1, 1, 1) * size;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void StartDie()
+    {
+        Dying = true;
+        StartCoroutine(Die());
+
+    }
+
+    IEnumerator Die() 
+    {
+        Color TargetColor = new Color(0.4f, 0.3f, 0.3f, 1);
+        Vector3 ColorDifference = new Vector3(TargetColor.r - StemRenderer.color.r, TargetColor.g - StemRenderer.color.g, TargetColor.b - StemRenderer.color.b);
+        float timer = 0;
+        while (MySpriteObject.transform.localScale.x > 0)
+        {
+            timer += Time.deltaTime * 2;
+            size -= Time.deltaTime * 2;
+            MySpriteObject.transform.localScale = new Vector3(1, 1, 1) * size;
+            yield return new WaitForEndOfFrame();
+            StemRenderer.color = new Color(
+                                            StemRenderer.color.r + ColorDifference.x * Time.deltaTime,
+                                            StemRenderer.color.g + ColorDifference.y * Time.deltaTime,
+                                            StemRenderer.color.b + ColorDifference.z * Time.deltaTime,
+                                            1
+                                          );
+            Debug.Log(StemRenderer.color);
+            MySpriteObject.transform.position += new Vector3(0,-1f,0) * Time.deltaTime * timer;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Vines/VinesSingle.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VinesSingle : MonoBehaviour
+{
+    [SerializeField]
+    float a;
+    [SerializeField]
+    float b;
+    [SerializeField]
+    float c;
+
+    float gap;
+    [SerializeField]
+    float length;
+    float ColorModifier;
+    [SerializeField]
+    private GameObject Segments;
+
+    [SerializeField]
+    private enum States { Spawning, Dying, Neutral }
+    private States MyStates;
+
+    [SerializeField]
+    List<GameObject> MySegs;
+
+    void Start()
+    {
+        MyStates = States.Neutral;
+        a = Random.Range(2, 7);
+        b = Random.Range(10, 15);
+        c = Random.Range(0f, 2f);
+        //SpawnVine();
+        ColorModifier = Random.Range(0f,0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            StartSpawnVine();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartKillVine();
+        }
+    }
+
+    void StartSpawnVine() 
+    {
+        StartCoroutine(SpawnVine());
+    }
+
+    void StartKillVine()
+    {
+        StartCoroutine(KillVine());
+    }
+
+    IEnumerator SpawnVine()
+    {
+        MyStates = States.Spawning;
+        float x = 0;
+        int count = Mathf.FloorToInt(length / gap);
+        for (int i = 0; x < length; i++)
+        {
+            if (MyStates != States.Spawning) 
+            {
+                break;
+            }
+
+            float size = Random.Range(0.2f, 1f);
+            gap = 0.12f * size;
+
+            x += gap;
+            float y = 0.07f * (Mathf.Sin(a * (x + c)) + 0.2f * Mathf.Sin(b * (x + c)));
+            //I assume that all cameras are set at -45 degrees;
+            //sqrt2 is about 1.414
+            //we also want to 
+            GameObject NextSegment = Instantiate(Segments, transform.position + new Vector3(x, y / 1.414f, y / 1.414f + -0.001f * (float)i), Segments.transform.rotation, transform);
+            Segments.transform.GetChild(0).GetComponent<SpriteRenderer>().color = new Color(0.3f + ColorModifier, 0.4f + ColorModifier, 0.2f, 1);
+            NextSegment.transform.localScale = new Vector3(size, size, size);
+            MySegs.Add(NextSegment);
+            yield return new WaitForSeconds(0.05f);
+        }
+        MyStates = States.Neutral;
+    }
+
+    IEnumerator KillVine()
+    {
+        MyStates = States.Dying;
+        for (int i = MySegs.Count - 1; i > -1; i--)
+        {
+            Debug.Log("aa");
+            MySegs[i].GetComponent<Segments>().StartDie();
+            MySegs.Remove(MySegs[i]);
+            yield return new WaitForSeconds(0.05f);
+        }
+        MyStates = States.Neutral;
+    }
+}
